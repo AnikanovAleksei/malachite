1216d080417b794d75a173f06973a201c0269137
Aleksei Anikanov <python_work@bk.ru>
2025-06-13 12:38:24 +0300

    Off

--- main.py
+++ main.py
@@ -35,16 +35,16 @@ async def send_scheduled_message(bot: Bot):
     image_path = '/root/malachite/image/IMG_1553.JPG'  # Укажите путь к вашему изображению
 
     # Используем InputFile для отправки изображения
-    photo = FSInputFile(image_path)
-
-    for user in users:
-        if user.telegram_id:
-            try:
-                await bot.send_photo(chat_id=user.telegram_id, photo=photo, caption=message_text)
-            except TelegramForbiddenError:
-                print(f"Bot was blocked by the user {user.telegram_id}")
-            except Exception as e:
-                print(f"Failed to send message to user {user.telegram_id}: {e}")
+    # photo = FSInputFile(image_path)
+    #
+    # for user in users:
+    #     if user.telegram_id:
+    #         try:
+    #             await bot.send_photo(chat_id=user.telegram_id, photo=photo, caption=message_text)
+    #         except TelegramForbiddenError:
+    #             print(f"Bot was blocked by the user {user.telegram_id}")
+    #         except Exception as e:
+    #             print(f"Failed to send message to user {user.telegram_id}: {e}")
 
 
 async def main():
@@ -59,14 +59,6 @@ async def main():
     dp.include_router(order_router)
     dp.include_router(helper_router)
     dp.include_router(manager_router)
-
-    # Проверка на существование задачи перед добавлением
-    if not scheduler.get_job("daily_post"):
-        # Настройка планировщика для выполнения задачи в 14:00
-        scheduler.add_job(send_scheduled_message, 'cron', hour=17, minute=5, args=[bot], id="daily_post")
-
-    # Запуск планировщика
-    scheduler.start()
 
     await dp.start_polling(bot)
 
